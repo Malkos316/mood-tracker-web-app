6d9ef98018c5588d50764645deaa3ac219cff2f2
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-11-22 18:38:14 -0700

    Update page.js

--- app/about/page.js
+++ app/about/page.js
@@ -11,8 +11,23 @@ export default function About() {
                 <h1 className='flex justify-center text-lg'>
                     About this WebApp
                 </h1>
-                <p className='flex justify-center'>
-                    This webapp was created to help people track their moods.
+                <p className='flex flex-col justify-center'>
+                    <p className='flex justify-center'>
+                        This webapp was created to help people track their moods.
+                        The Idea is this:
+                    </p>
+                    <p className='flex justify-center'>
+                        login using OAUTH and a google account.
+                    </p>
+                    <p className='flex justify-center'>
+                        Home page:
+                    </p>
+                    <p className='flex justify-center'>
+                        The home page should have a calendar, with each day having a mood rating.
+                    </p>
+                    <p className='flex justify-center'>
+                        the user should be able to scroll through the months and years to see their mood history.
+                    </p>
                 </p>
             </div>
         </main>
