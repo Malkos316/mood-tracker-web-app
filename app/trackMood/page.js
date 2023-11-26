cc4d2acbbfbd00bd00f83ae5d4ef0fc0dc18afc3
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-11-26 12:48:26 -0700

    Update page.js

--- app/trackMood/page.js
+++ app/trackMood/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react';
 import { header } from '../components/header';
+import MoodLevel from '../components/moodLevel';
 import MonthViewCalendar from '../components/monthViewCalendar';
 
 export default function Home() {
