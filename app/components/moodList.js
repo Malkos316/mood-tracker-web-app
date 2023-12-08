35f65c25ab98154068fbd7b43b27f533b46a00d0
Malkos316 <118082476+Malkos316@users.noreply.github.com>
2023-12-08 14:34:55 -0700

    Update moodList.js
    working

--- app/components/moodList.js
+++ app/components/moodList.js
@@ -16,30 +16,37 @@ const MoodList = () => {
 
     const handleDelete = (date) => {
         if (window.confirm('Are you sure you want to delete this item?')) {
-            delete moodData[date];
-            localStorage.setItem('moodData', JSON.stringify(moodData));
-            loadMoodData();
+            const newMoodData = { ...moodData };
+            delete newMoodData[date];
+            localStorage.setItem('moodData', JSON.stringify(newMoodData));
+            setMoodData(newMoodData);
         }
     };
+
+    // Convert object to array, sort it by date in descending order and map to render the list
+    const sortedMoodEntries = Object.entries(moodData).sort((a, b) => {
+        // Convert dates from string to Date objects, then sort
+        return new Date(b[0]) - new Date(a[0]);
+    });
 
     return (
         <div>
             <h2 className='flex justify-center text-4xl mt-2'>Mood Data</h2>
             <ul className='flex flex-col items-center'>
-                {Object.entries(moodData).map(([date, data], index) => (
+                {sortedMoodEntries.map(([date, data], index) => (
                     <li key={index}
                         className='flex justify-between items-center border-4 border-primary mt-2 w-full px-4 py-2'>
                         <div className='overflow-auto break-words'>
-                            <strong>Date: </strong> {new Date(date).toLocaleDateString() }
+                            <strong>Date: </strong> {new Date(date).toLocaleDateString()}
                             <br />
-                            <strong>Mood Level: </strong> {data?.moodLevel }
+                            <strong>Mood Level: </strong> {data?.moodLevel}
                             <br />
-                            <strong>Emotions: </strong> {data?.emotions?.join(", ") }
+                            <strong>Emotions: </strong> {data?.emotions?.join(", ")}
                             <br />
                             <strong>Description: </strong> {data?.description}
                         </div>
                         <button onClick={() => handleDelete(date)}
-                                className=' p-2 m-2 cursor-pointer text-white rounded hover:bg-secondary-200 bg-secondary-100'>Delete</button>
+                                className='p-2 m-2 cursor-pointer text-white rounded hover:bg-secondary-200 bg-secondary-100'>Delete</button>
                     </li>
                 ))}
             </ul>
